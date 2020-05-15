4f93d4dc9b4cb8075ce4de4f082aed9b39e3c9ac
Tyler Firth <tyfirth@gmail.com>
2020-05-15 16:45:43 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -51,9 +51,8 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-holiday_hash[:winter].each do |holiday, decoration|
-  holiday.sort
-end
+holiday_hash[:winter].values.flatten
+
 end
 
 def all_supplies_in_holidays(holiday_hash)
