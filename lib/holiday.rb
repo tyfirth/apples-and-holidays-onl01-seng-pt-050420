d33c8932686426b6e5916f98bb420699be44b598
Tyler Firth <tyfirth@gmail.com>
2020-05-15 16:14:55 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,6 +18,13 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  
+  holiday_hash.each do |season, holiday|
+    if season == :summer
+      holiday.each do |
+  end
+  end
+  
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
