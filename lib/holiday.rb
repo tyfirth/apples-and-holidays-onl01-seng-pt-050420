bf15812feff6d8a83d38f623f37734c627fef9d5
Tyler Firth <tyfirth@gmail.com>
2020-05-15 16:23:12 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -27,6 +27,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
+  
 holiday_hash[:winter][:christmas], holiday_hash[:winter][:new_years] << "Balloons"
 end
 
