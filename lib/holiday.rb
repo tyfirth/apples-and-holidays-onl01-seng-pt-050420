654d15e73c5782aefa6a42a7e5b559a86d2b9a50
Tyler Firth <tyfirth@gmail.com>
2020-05-15 17:25:45 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -77,7 +77,7 @@ def all_holidays_with_bbq(holiday_hash)
  
  holiday_hash.map do |season, holiday|
    holiday.map do |holiday, item|
-     if item.include?("BBQ")
+    holiday if item.include?("BBQ")
        puts 
    end
  end
