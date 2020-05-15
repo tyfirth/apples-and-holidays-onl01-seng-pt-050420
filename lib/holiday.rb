eef6374d622275c4a39fa65c9e2b8522fa685b55
Tyler Firth <tyfirth@gmail.com>
2020-05-15 17:02:43 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,7 +66,7 @@ def all_supplies_in_holidays(holiday_hash)
 holiday_hash.each do |season, holiday|
   puts "#{season.capitalize}:"
   holiday.each do |holiday, item|
-    puts " #{holiday}.to_s.map {|i| i.capitalize}.join(" ") "
+    puts " #{holiday}.to_s.map {|i| i.capitalize}.join(" ")}: "
   end
 end
 end
